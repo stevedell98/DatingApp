cd3df6115b0ca2e43c5ea5b2d2513a3010177197
Stefanos <stevedel98@gmail.com>
2023-07-11 01:21:24 +0300

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -14,10 +14,13 @@ opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
 
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();  //Tells to our request, which API endpoint to go for
 
 app.Run();
